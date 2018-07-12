417bea968caff9c3e3cb3334096512931bc7215b
Ravi Nekkanti <ravi.nekkanti@macys.com>
2018-07-12 16:19:19 +0530

    selenium changes

--- src/main/java/seleniumframework/TestClass.java
+++ src/main/java/seleniumframework/TestClass.java
@@ -1,9 +1,14 @@
 package seleniumframework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestClass {
 
 	public static void main(String[] args) {
-
+		WebDriver driver;
+		driver=new  ChromeDriver();
+		driver.get("http://mail.yahoo.com");
 	}
 
 }
--- src/main/java/seleniumframework/TestClass1.java
+++ src/main/java/seleniumframework/TestClass1.java
@@ -3,6 +3,8 @@ package seleniumframework;
 public class TestClass1 {
 
 	public static void main(String[] args) {
+		
+	System.out.println("hello");
 		// TODO Auto-generated method stub
 
 	}
