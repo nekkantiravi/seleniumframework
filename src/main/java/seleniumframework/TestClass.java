1387371fe062bb681c9ee044a1d22cc9ae12825a
Ravi Nekkanti <ravi.nekkanti@macys.com>
2018-07-13 14:57:06 +0530

    added all 3 files

--- src/main/java/seleniumframework/TestClass.java
+++ src/main/java/seleniumframework/TestClass.java
@@ -1,7 +1,15 @@
 package seleniumframework;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestClass {
 
@@ -9,6 +17,28 @@ public class TestClass {
 		WebDriver driver;
 		driver=new  ChromeDriver();
 		driver.get("http://mail.yahoo.com");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
+		WebDriverWait wait = new WebDriverWait(driver, 60 );
+		    wait.until(ExpectedConditions.elementToBeSelected(driver.findElement(By.xpath(""))));
+		
+
+			WebDriverWait wait1 = new WebDriverWait(driver, 60 );
+			wait1.pollingEvery(5,TimeUnit.SECONDS);
+			wait1.until(ExpectedConditions.elementToBeSelected(driver.findElement(By.tagName("\\q"))));
+			
+			
+			Alert alert = driver.switchTo().alert();
+			alert.accept();
+			alert.dismiss();
+			
+			driver.findElements(By.tagName("")).size();
+			
+			driver.switchTo().frame(3);
+			driver.switchTo().frame("auto");
+			driver.switchTo().frame(driver.findElement(By.className("ravi")).getAttribute(""));
+			
+		
 	}
 
 }
--- src/main/java/seleniumframework/TestClass1.java
+++ src/main/java/seleniumframework/TestClass1.java
@@ -5,7 +5,6 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		
 	System.out.println("hello");
-		// TODO Auto-generated method stub
 
 	}
 
